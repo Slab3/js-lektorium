a5ef64f49e2ddb6de2d0a4aa741aa662f7ca5b78
Slab3 <zshylga@gmail.com>
2021-09-19 22:21:10 +0300

    init main func

--- two-dimensional-array/scripts/main.js
+++ two-dimensional-array/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('okay');
+function twoDimensArr(columnAmount, rowAmount, columnStart, rowStart, direction = 'left') {
+
+  const totalCellAmount = columnAmount * rowAmount;
+  let counter = 1;
+
+  // const matrix = Array.from({length: totalCellAmount}, () => Array(totalCellAmount).fill(counter));
+
+  // return matrix;
+}
+
+console.log(twoDimensArr(5,6));
+
